53c05c40dd82949a094c5d854462b45f7c6d260a
Frit027 <Frit_027@mail.ru>
2023-05-14 16:23:16 +0300

    Added function to check age

--- main.py
+++ main.py
@@ -19,6 +19,20 @@ def correct_name(query):
         return EMPTY_TEXT
 
 
+# Функция для исправления возраста (второго поля)
+def correct_age(query):
+    # Удаляем из строки все, что не является цифрами,
+    # Примеры:
+    # Было: '4xy5a', стало: '45'
+    # Было: 'abc',   стало: ' - '
+
+    corrected = re.sub(r'\D+', '', query)
+    if len(corrected) > 0:
+        return corrected
+    else:
+        return EMPTY_TEXT
+
+
 if __name__ == '__main__':
     with open('input.txt', 'r', encoding='utf-8') as file:
         lines = file.readlines()
