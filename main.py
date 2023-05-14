309c0fac0b12ed6d27a4c5c2115f826fc6f32336
Frit027 <Frit_027@mail.ru>
2023-05-14 16:25:14 +0300

    Added function to check phone number

--- main.py
+++ main.py
@@ -33,6 +33,27 @@ def correct_age(query):
         return EMPTY_TEXT
 
 
+# Функция для исправления номера телефона (третьего поля)
+def correct_phone(query):
+    # Удаляем все символы кроме цифр и смотрим, сколько получилось цифр в итоге.
+    # В номере телефона их должно быть 11, при этом первая из них должна быть либо 7, либо 8.
+    # Если цифр меньше или больше 11, либо если первая из них не 7 или 8, то номер
+    # можно считать некорректным, поэтому возвращаем пустую строку.
+    # Примеры:
+    # Было: '85674x3y65267', стало: '+7 (567) 436-52-67'
+    # Было: '123456',        стало: ' - '
+
+    corrected = re.sub(r'\D+', '', query)
+    if len(corrected) == 11 and corrected[0] in '78':
+        part1 = corrected[1:4]
+        part2 = corrected[4:7]
+        part3 = corrected[7:9]
+        part4 = corrected[9:11]
+        return f'+7 ({part1}) {part2}-{part3}-{part4}'
+    else:
+        return EMPTY_TEXT
+
+
 if __name__ == '__main__':
     with open('input.txt', 'r', encoding='utf-8') as file:
         lines = file.readlines()
