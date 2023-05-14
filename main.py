8629896525af7ad63da662e161260f86a182fd1d
Frit027 <Frit_027@mail.ru>
2023-05-14 16:19:23 +0300

    Reading a file, checking the number of fields in a line, splitting a line into fields

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    with open('input.txt', 'r', encoding='utf-8') as file:
+        lines = file.readlines()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    with open('output.txt', 'w', encoding='utf-8') as file:
+        for line in lines:
+            line_content = line.rstrip().split('|')
+            if len(line_content) != 4:
+                file.write('Некорректная строка\n')
+                continue
+            name, age, phone, email = line_content
