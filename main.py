d40ee5239e4e6bcd8119641a1f108f9cb54174b1
Frit027 <Frit_027@mail.ru>
2023-05-14 17:27:30 +0300

    test GitHub actions

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ if __name__ == '__main__':
             email = correct_email(email)
 
             corrected_line = '|'.join((name, age, phone, email))
+
             file.write(f'{corrected_line}\n')
 
 print('Файл исправлен и сохранен как output.txt')
